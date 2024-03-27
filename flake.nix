752771490fd22e821b4eb730a5a4deb693857f98
Jamie Atkinson <Jabbslad@users.noreply.github.com>
2024-03-27 01:50:41 +0000

    v0.33.0 --> v1.0.0 (#10)

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    gleam.url = "github:gleam-lang/gleam/v0.33.0";
+    gleam.url = "github:gleam-lang/gleam/v1.0.0";
     gleam.flake = false;
 
     flake-compat.url = "github:edolstra/flake-compat";
